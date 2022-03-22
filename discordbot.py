f0203dd045a015e7b1a08b4fac5ec1152fc0d915
Bui Bao Hoang <89177013+buibaohoang06@users.noreply.github.com>
2022-03-22 18:24:03 +0700

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -134,7 +134,7 @@ async def scam(ctx, name):
     ]
     await ctx.channel.send(name + messages[random.randint(0, len(messages))] + "\nClick on this link! Link: https://www.thisworldthesedays.com/free-stuff4.html")
 @bot.command(pass_context = True)
-@has_permissions(kick_member = True)
+@has_permissions(kick_members = True)
 async def kick(ctx, username: User):
 	if target.server_permissions.administrator:
 		await ctx.channel.send("Targeted user is an Admin!")
